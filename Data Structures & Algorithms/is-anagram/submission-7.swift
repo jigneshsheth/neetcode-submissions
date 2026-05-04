6ca85388337eb0f1fa0d35885a2985b24c2d8fed
Jigs <jigneshsheth@users.noreply.github.com>
2026-05-04 00:44:12 -0500

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+      if s.count != t.count {
+         return false
+      }
+
+      return s.sorted() == t.sorted()
+    }
+}
